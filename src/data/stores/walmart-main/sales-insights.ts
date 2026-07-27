5d738a683159a574e5dbc902f8e03a93410c484f
Solomon <primesolomon95@gmail.com>
2026-07-27 17:02:58 +0100

    Update files to July 27

--- src/data/stores/walmart-main/sales-insights.ts
+++ src/data/stores/walmart-main/sales-insights.ts
@@ -14,8 +14,8 @@ export const walmartMainDataConfig: Omit<
   timeSeriesSeed: 99,
   timeSeriesProfile: "spike-collapse",
   rangeStart: "2024-01-01",
-  rangeEnd: "2026-07-23",
-  targetSales: 345989.38,
+  rangeEnd: "2026-07-27",
+  targetSales: 347719.33,
 };
 
 function buildTableRows(
